88ca3f6bd6912e7e907441e3450480f2f866a16b
zhangdezhi <1541609448@qq.com>
2020-12-28 23:10:41 +0800

    添加页面

--- src/renderer/pages/home/views/index.tsx
+++ src/renderer/pages/home/views/index.tsx
@@ -1,15 +1,23 @@
 import { connect } from 'dva';
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Layout } from 'antd';
+import styles from '../styles/index.less'
+
+const { Header, Content, Footer, Sider } = Layout;
 
 export default class Index extends Component<any,any> {
   
 
   render() {
     return (
-      <div>
-        <Button type="primary">hello</Button>
-      </div>
+        <Layout className={styles.layout}>
+        <Header className={styles.header}>Header</Header>
+        <Layout>
+          <Sider className={styles.sider}>Sider</Sider>
+          <Content className={styles.content}>Content</Content>
+        </Layout>
+        {/* <Footer className={styles.footer}>Footer</Footer> */}
+      </Layout>
     );
   }
 }
