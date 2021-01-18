c3d6a1aee6672742594761a3de4ed0c3454858d3
zhangdezhi <1541609448@qq.com>
2021-01-18 18:05:00 +0800

    完善登录功能

--- src/renderer/pages/login/views/index.tsx
+++ src/renderer/pages/login/views/index.tsx
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import { connect } from 'dva'
 import { Link } from 'dva/router';
 import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import styles from '../styles/index.less';
 
 
 
 const Login  = () => {
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
   return (
     <div className={styles.container}>
     <div className={styles.login}>
@@ -15,37 +19,37 @@ const Login  = () => {
       </div>
       <div className={styles.right}>
         <p className={styles.logo}>
-          {/* <a><img alt='前端临控平台' src={require('../../../assets/loginlogo.png')} /></a> */}
+          <a>前端临控平台</a>
         </p>
-        {/* <Form onSubmit={this.handleSubmit} className={styles.loginFrom}>
-          <Form.Item>
-            {getFieldDecorator('loginName', {
-              rules: [{ required: true, message: '请输入账号!' }],
-            })(
-              <Input addonBefore={<img alt='' src={require('../../../assets/user.png')} className={styles.prefixIcon} />} placeholder="输入系统账号" />
-            )}
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+        <Form.Item
+          name="email"
+          rules={[{ required: true, message: '邮箱不能为空！' }]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="请输入邮箱" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: '密码不能为空！' }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="请输入密码"
+            />
           </Form.Item>
           <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: '请输入密码!' }],
-            })(
-              <Input addonBefore={<img alt='' src={require('../../../assets/lock.png')} theme="filled" className={styles.prefixIcon} />} type="password" placeholder="输入登录密码" />
-            )}
-            {
-              this.props.login.msg === "" || this.props.login.msg === undefined ?
-                null
-                :
-                <div className='ant-form-explain' style={{ position: 'absolute', color: '#f5222d', zIndex: '12', backgroundColor: '#fff', top: '30px' }}>{this.props.login.msg}</div>
-            }
+            <Button type="primary" htmlType="submit" className={styles.submit}>
+             登 录
+            </Button>
           </Form.Item>
-          <Form.Item>
-            <Button disabled={this.state.buttonStatus} htmlType="submit" className={styles.submit} size="large">
-              登录
-           </Button>
-          </Form.Item>
-        </Form> */}
+        </Form>
       </div>
-
     </div>
   </div>
   )
@@ -53,91 +57,3 @@ const Login  = () => {
 
 export default Login;
 
-
-// interface LoginProps {
-//   form:any;
-//   dispatch:any;
-
-// }
-
-// interface LoginState{
-//   buttonStatus:boolean;
-// }
-
-// class Login extends Component<LoginProps,LoginState> {
-//   state = {
-//     buttonStatus: false
-//   }
-
-//   handleSubmit = (e) => {
-//     const { dispatch } = this.props
-//     e.preventDefault();
-//     this.props.form.validateFields((err, values) => {
-//       if (!err) {
-//         this.setState({
-//           buttonStatus: true
-//         })
-//         dispatch({ type: 'login/login', payload: values })
-//         setTimeout(() => {
-//           this.setState({
-//             buttonStatus: false
-//           })
-//         }, 3000)
-//       }
-//     });
-//   }
-
-//   render() {
-//     // const { getFieldDecorator } = this.props.form;
-//     return (
-      // <div className={styles.container}>
-      //   <div className={styles.login}>
-      //     <div className={styles.left}>
-      //       <p className={styles.title}>欢迎登录</p>
-      //     </div>
-      //     <div className={styles.right}>
-      //       <p className={styles.logo}>
-      //         <a><img alt='前端临控平台' src={require('../../../assets/loginlogo.png')} /></a>
-      //       </p>
-      //       {/* <Form onSubmit={this.handleSubmit} className={styles.loginFrom}>
-      //         <Form.Item>
-      //           {getFieldDecorator('loginName', {
-      //             rules: [{ required: true, message: '请输入账号!' }],
-      //           })(
-      //             <Input addonBefore={<img alt='' src={require('../../../assets/user.png')} className={styles.prefixIcon} />} placeholder="输入系统账号" />
-      //           )}
-      //         </Form.Item>
-      //         <Form.Item>
-      //           {getFieldDecorator('password', {
-      //             rules: [{ required: true, message: '请输入密码!' }],
-      //           })(
-      //             <Input addonBefore={<img alt='' src={require('../../../assets/lock.png')} theme="filled" className={styles.prefixIcon} />} type="password" placeholder="输入登录密码" />
-      //           )}
-      //           {
-      //             this.props.login.msg === "" || this.props.login.msg === undefined ?
-      //               null
-      //               :
-      //               <div className='ant-form-explain' style={{ position: 'absolute', color: '#f5222d', zIndex: '12', backgroundColor: '#fff', top: '30px' }}>{this.props.login.msg}</div>
-      //           }
-      //         </Form.Item>
-      //         <Form.Item>
-      //           <Button disabled={this.state.buttonStatus} htmlType="submit" className={styles.submit} size="large">
-      //             登录
-      //          </Button>
-      //         </Form.Item>
-      //       </Form> */}
-      //     </div>
-
-      //   </div>
-      // </div>
-
-//     );
-//   }
-// }
-
-// export default connect(Login);
-
-
-// export default Form.create()(Login);
-
-// export default connect(({ login }) => ({ login }))();
