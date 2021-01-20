08af5b57e12c2084e668816ea423dcf66be97291
zhangdezhi <1541609448@qq.com>
2021-01-20 14:01:46 +0800

    添加文档

--- src/renderer/pages/app.tsx
+++ src/renderer/pages/app.tsx
@@ -6,13 +6,7 @@ import styles from './app.less';
 const { Header, Content,Sider } = Layout;
 
 export default class Index extends Component<any,any> {
-  
-
   render() {
-    const files = [
-      { title: 'Tab 1', content: 'Content of Tab Pane 1', key: '1' },
-      { title: 'Tab 2', content: 'Content of Tab Pane 2', key: '2' },
-    ];
     return (
         <Layout className={styles.layout}>
         <Header className={styles.header}>Header</Header>
